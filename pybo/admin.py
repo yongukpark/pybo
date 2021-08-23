80638695e518284e433acd5e920f661ba07de8c3
박용욱 <bobby3400@naver.com>
2021-08-23 19:18:02 +0900

    첫 프로젝트

---
+++ pybo/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Question
+from .models import Answer
+
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['subject']
+
+admin.site.register(Question , QuestionAdmin)
+admin.site.register(Answer)
+# Register your models here.
